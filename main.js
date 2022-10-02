82f6be218d2681fe47e48f713afe6915d8f5a305
Karenna Ng <karenna@MacBook-Air-3.local>
2022-10-02 11:59:35 -0400

    circle hover and click functions, last point clicked

--- main.js
+++ main.js
@@ -12,19 +12,16 @@ function circleUnhover(circle) {
 }
 
 // circle click function
-let clicks = 0;
-
 function circleSelect(circle) {
 	let c = document.getElementById(circle.target.id);
 	let textDiv = document.getElementById("last-selected");
-	if (c.classList.contains("selected") && clicks > 0) {
+	if (c.classList.contains("selected")) {
 		c.classList.remove("selected");
 	}
 	else {
 		c.classList.add("selected");
 	}
 	textDiv.innerHTML = "Last point clicked: " + circle.target.id;
-	clicks += 1;
 }
 
 // parse through circles and apply hover and click functions
